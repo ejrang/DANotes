239a8eb738f2ebd7508f4aaf1344ef18c7024c48
Ejran <ejrannahimzadah@gmail.com>
2025-02-09 16:36:43 +0100

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-2e516","appId":"1:535067045578:web:6a7a9c3fafcb434ebf0e33","storageBucket":"danotes-2e516.firebasestorage.app","apiKey":"AIzaSyAQjoACR9YeNmaArlEoS7Z91e9_t7KW-4M","authDomain":"danotes-2e516.firebaseapp.com","messagingSenderId":"535067045578","measurementId":"G-JW5HCDZEBZ"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+};
